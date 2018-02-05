d2372637b480c4e95dff4846515221be236cc023
barclay loftus <barclay@deliv.co>
2018-02-05 11:28:29 -0800

    Adding in codeclimate, and simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,9 @@
+require 'simplecov'
+require 'simplecov-json'
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::JSONFormatter,
+]
+
+SimpleCov.start
 require 'nhtsa_vin'
